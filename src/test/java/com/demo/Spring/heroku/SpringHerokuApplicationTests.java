0ee3b3e495a928dbb7eafe8af0b61004a9121b94
Artur Kubiak <arturkubiak9@gmail.com>
2022-01-30 13:24:23 +0100

    first commit

---
+++ src/test/java/com/demo/Spring/heroku/SpringHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Spring.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
